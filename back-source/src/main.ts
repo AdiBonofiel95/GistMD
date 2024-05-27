417ec63bfa70dbe9e7a102ff6eba0f80b0731350
AdiBonofiel95 <adibonofiel@gmail.com>
2024-05-27 14:18:00 +0300

    removed tests and added another env variable

--- back-source/src/main.ts
+++ back-source/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin:[
-      'http://localhost:3001',
+      process.env.CLIENT_LOCAL_HOST,
     ]
   })
   await app.listen(process.env.PORT);
